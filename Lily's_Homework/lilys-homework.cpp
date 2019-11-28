436dabb9af593552441893f20a8551dba9c8b7c2
Alex Liao <gradop09@gmail.com>
2019-11-28 17:54:02 +0800

    Initial HackerRank repo commit

---
+++ Lily's_Homework/lilys-homework.cpp
@@ -0,0 +1,14 @@
+// Problem: Lily's Homework
+
+// Algorithm:
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// Complete the lilysHomework function below.
+int lilysHomework(vector<int> arr) {
+
+
+}
